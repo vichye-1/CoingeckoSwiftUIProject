f75a5b571799e8f21f779a5ddb3360c231c0f7b0
knockknock <yangban11111@gmail.com>
2024-09-06 20:48:28 +0900

    Initial Commit

---
+++ CoingeckoSwiftUIProject/CoingeckoSwiftUIProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoingeckoSwiftUIProjectApp.swift
+//  CoingeckoSwiftUIProject
+//
+//  Created by 양승혜 on 9/6/24.
+//
+
+import SwiftUI
+
+@main
+struct CoingeckoSwiftUIProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
